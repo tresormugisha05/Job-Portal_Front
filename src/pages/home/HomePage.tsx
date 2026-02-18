b8efdd1aa8716f584d28805492ef761287b9db2c
tresor <tresormugisha07@gmail.com>
2026-02-18 08:59:42 +0200

    the last version

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,10 +1,10 @@
-import PageWrapper from "../shared/layouts/PageWrapper";
-import HomeHero from "./home/HomeHero";
-import HomeJobs from "./home/HomeJobs";
-import HomeCompanies from "./home/HomeCompanies";
-import HomeResumes from "./home/HomeResumes";
-import Loader from "../shared/components/ui/Loader";
-import usePageLoader from "../shared/components/hooks/usePageLoader";
+import PageWrapper from "../../shared/layouts/PageWrapper";
+import HomeHero from "./HomeHero";
+import HomeJobs from "./HomeJobs";
+import HomeCompanies from "./HomeCompanies";
+import HomeResumes from "./HomeResumes";
+import Loader from "../../shared/components/ui/Loader";
+import usePageLoader from "../../shared/components/hooks/usePageLoader";
 export default function HomePage() {
   const isLoading = usePageLoader(1000);
 
